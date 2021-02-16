c333c88985e2d1ff3995d8cb7543a9fbdfa1bc34
AlvaroGD99 <alv.gonz.del@gmail.com>
2021-02-16 21:13:12 +0100

    Creación de rutas, service, interface y components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponentComponent } from './blog-component/blog-component.component';
+import { FormularioComponentComponent } from './formulario-component/formulario-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'blog', component: BlogComponentComponent },
+  { path: 'new', component: FormularioComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
